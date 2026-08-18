243b85d5a4bfb4ae9a82adaa19a1038a0f9eb0f0
Pietra <pietragomesdonato@gmail.com>
2026-08-18 14:18:32 -0300

    teste

---
+++ testinggitclone.js
@@ -0,0 +1,9 @@
+//Array
+//para fazer uma Array no JS declaramos uma variavel com []
+let listaDeExemplo = ['Pedro','Jõao', 'Roberta'];
+// string sempre com '' e números não utilizam ''
+let listaNumeros = [1, 2, 3, 4];
+//para ver o tamanho de uma lista utilizamos o comando: length
+//nomeDaLista.lenght
+console.log(listaDeExemplo.length);
+
